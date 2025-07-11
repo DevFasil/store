168b66711697ae92cfd517cced88f6860a3ca337
admassufasil <admassufasil@gmail.com>
2025-07-11 13:49:33 -0500

    Feature change for testing

--- src/main/java/com/devFlow/devFlow/Controller/UserController.java
+++ src/main/java/com/devFlow/devFlow/Controller/UserController.java
@@ -1,15 +1,12 @@
 package com.devFlow.devFlow.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
-    @RequestMapping("/user")
-
-    public String user() {
-        System.out.println("Feature change for testing purposes");
-        return "User endpoint reached!";
+    @GetMapping("/user")
+    public String getUser() {
+        return "User details";
     }
 }
