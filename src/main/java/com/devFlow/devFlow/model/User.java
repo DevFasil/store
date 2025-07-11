16c7e666309a8ac1d11335c0b8a02fc95837f8c3
admassufasil <admassufasil@gmail.com>
2025-07-11 11:36:10 -0500

    Add annotations and new role class

--- src/main/java/com/devFlow/devFlow/model/User.java
+++ src/main/java/com/devFlow/devFlow/model/User.java
@@ -1,6 +1,14 @@
 package com.devFlow.devFlow.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
     private Long id;
     private Long createdBy;
     private Long updatedBy;
